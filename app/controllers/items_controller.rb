11a128825c7122c16a8b073129a528a273d1941e
Lucas S <lucas.santos.silva2011@gmail.com>
2022-07-17 22:09:00 -0300

    Remove comments and unused code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,20 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show edit update destroy ]
-
-  # GET /items or /items.json
   def index
     @items = Item.all
   end
 
-  # GET /items/1 or /items/1.json
   def show
+    @item = Item.find(params[:id])
   end
 
-  # GET /items/new
   def new
     @item = Item.new
   end
 
-  # GET /items/1/edit
   def edit
+    @item = Item.find(params[:id])
   end
 
-  # POST /items or /items.json
   def create
     @item = Item.new(item_params)
 
@@ -34,8 +29,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1 or /items/1.json
   def update
+    @item = Item.find(params[:id])
+
     respond_to do |format|
       if @item.update(item_params)
         format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
@@ -47,9 +43,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1 or /items/1.json
   def destroy
-    @item.destroy
+    Item.find(params[:id]).destroy
 
     respond_to do |format|
       format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
@@ -58,13 +53,8 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def item_params
-      params.require(:item).permit(:description, :done)
-    end
+  def item_params
+    params.require(:item).permit(:description, :done)
+  end
 end
