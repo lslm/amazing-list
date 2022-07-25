95e54e1c2f868c50767f41f9e001fea613cb544f
Lucas S <lucas.santos.silva2011@gmail.com>
2022-07-24 22:28:31 -0300

    Scoping boards listing

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @boards = Board.all
+    @boards = current_user.boards.all
   end
 
   def show
